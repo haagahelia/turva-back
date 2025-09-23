4b44a79b6c7a4bc2b7750ac91f3f94c6ebaaaaa8
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-09-23 16:54:22 +0300

    feat(CRUD): display all database content as json with GET /api/info

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { pool } from "../db";
 
 const router = Router();
 
@@ -8,6 +9,17 @@ router.get("/ping", (_req, res) => {
 
 router.get("/", (_req, res) => {
     res.json("Hello world!")
+});
+
+router.get("/info", async (_req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM info ORDER BY id;");
+        res.json(result.rows);
+    } catch (err) {
+        console.error("Error in query:", err);
+        res.status(500).json({ error: "Database query failed" });
+
+    }
 });
 
 export default router;
