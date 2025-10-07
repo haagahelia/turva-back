c0e677615e69105274153764c2bcdc2bdf8a2991
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-10-07 19:00:25 +0300

    fix(api): show error msg when GET /info/:id is not found, small edits to styling

--- src/routes/info.ts
+++ src/routes/info.ts
@@ -20,6 +20,10 @@ router.get("/:id", async (req: Request, res: Response): Promise<void> => {
             "SELECT * FROM info WHERE id = $1",
             [id]
         );
+        if (result.rowCount === 0) {
+            res.status(404).json({ error: "ID not found" });
+            return;
+        };
         res.json(result.rows);
     } catch (err) {
         console.error("Error in query:", err);
@@ -34,13 +38,11 @@ router.delete("/:id", async (req: Request, res: Response): Promise<void> => {
             "DELETE FROM info WHERE id = $1 RETURNING *",
             [id]
         );
-
         if (result.rowCount === 0) {
             res.status(404).json({ error: "ID not found" });
             return;
-        }
-
-        res.json({ message: "Deleted successfully", deleted: result.rows[0] })
+        };
+        res.json({ message: "Deleted successfully", deleted: result.rows[0] });
     } catch (err) {
         console.error("Delete failed", err);
         res.status(500).json({ error: "Delete failed" });
@@ -50,16 +52,14 @@ router.delete("/:id", async (req: Request, res: Response): Promise<void> => {
 router.post("/", async (req: Request, res: Response): Promise<void> => {
     try {
         const { title, content } = req.body;
-
         if (!title || !content) {
             res.status(400).json({ error: "Title or content missing" });
             return;
-        }
-
+        };
         const result = await pool.query(
-            "INSERT INTO info (title, content) VALUES ($1, $2) RETURNING *", [title, content]
+            "INSERT INTO info (title, content) VALUES ($1, $2) RETURNING *",
+            [title, content]
         );
-
         res.status(201).json(result.rows[0]);
     } catch (err) {
         console.error("Insert failed", err);
@@ -71,27 +71,23 @@ router.put("/:id", async (req: Request, res: Response): Promise<void> => {
     try {
         const { id } = req.params;
         const { title, content } = req.body;
-
         if (!title || !content) {
             res.status(400).json({ error: "Title or content missing" });
             return;
-        }
-
+        };
         const result = await pool.query(
-            "UPDATE info SET title = $1, content = $2, created_at = CURRENT_TIMESTAMP WHERE id = $3 RETURNING *", [title, content, id]
+            "UPDATE info SET title = $1, content = $2, created_at = CURRENT_TIMESTAMP WHERE id = $3 RETURNING *",
+            [title, content, id]
         );
-
         if (result.rowCount === 0) {
             res.status(400).json({ error: "ID not found" });
             return;
-        }
-
+        };
         res.json({ message: "Updated successfully", updated: result.rows[0] });
     } catch (err) {
         console.error("Update failed:", err);
         res.status(500).json({ error: "Update failed" });
     }
-
 });
 
 export default router;
