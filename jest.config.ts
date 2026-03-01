4ea0202dbb35e95089d6384898487b2d934cafca
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2026-03-01 18:37:56 +0200

    test: add jest setup file for environment configuration

--- jest.config.ts
+++ jest.config.ts
@@ -6,32 +6,33 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  setupFiles: ["dotenv/config"],
-  clearMocks: true,
-  collectCoverage: false,
-  coverageDirectory: "coverage",
-  roots: [
-    "<rootDir>"
-  ],
-  testMatch: [
-    "**/__tests__/**/*.?([mc])[jt]s?(x)",
-    "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"
-  ],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-  moduleFileExtensions: ["js", "ts"],
-  testEnvironment: "node",
-  transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      useESM: true,
-      tsconfig: './tsconfig.json'
-    }],
-  },
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+	setupFiles: ['dotenv/config'],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	clearMocks: true,
+	collectCoverage: false,
+	coverageDirectory: 'coverage',
+	roots: ['<rootDir>'],
+	testMatch: [
+		'**/__tests__/**/*.?([mc])[jt]s?(x)',
+		'**/?(*.)+(spec|test).?([mc])[jt]s?(x)',
+	],
+	testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+	moduleFileExtensions: ['js', 'ts'],
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.ts$': [
+			'ts-jest',
+			{
+				useESM: true,
+				tsconfig: './tsconfig.json',
+			},
+		],
+	},
+	moduleNameMapper: {
+		'^src/(.*)$': '<rootDir>/src/$1',
+		'^@/(.*)$': '<rootDir>/src/$1',
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	},
 };
-
 
 export default config;
